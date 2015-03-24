319468efacc0e353bc707b7d2675ca1fcf2fdd56
Paul Matuzak <philopdx@gmail.com>
2015-03-24 16:24:38 -0700

    Dollar Sign Widget

--- boots/forms.py
+++ boots/forms.py
@@ -7,6 +7,7 @@ class DemoForm(forms.Form):
     date_range = boot_fields.DateRangeField()
     calendar = boot_fields.CalendarDateField()
     name = boot_fields.AtSymbolInputField()
+    money = boot_fields.DollarSignField()
 
 
 
