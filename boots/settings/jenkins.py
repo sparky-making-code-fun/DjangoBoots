4a28e86e93069bbaec6e6aa90c61872740a076f5
Daniel Avila (sparky) <sparkytwobillion@gmail.com>
2015-04-04 11:34:25 -0700

    Getting jenkins setup and running

--- boots/settings/jenkins.py
+++ boots/settings/jenkins.py
@@ -1,1 +1,19 @@
 from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS = INSTALLED_APPS + ('django_jenkins',)
+
+
+JENKINS_TASKS = (
+            'django_jenkins.tasks.run_pep8',
+            'django_jenkins.tasks.run_pyflakes',
+            'django_jenkins.tasks.run_jshint',
+            'django_jenkins.tasks.run_csslint',
+            'django_jenkins.tasks.run_sloccount'
+)
