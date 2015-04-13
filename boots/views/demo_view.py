e7df9fab71c9a1e7be9424d4341934b3111534b0
Daniel Avila <Daniel-Avila@users.noreply.github.com>
2015-04-13 15:22:20 -0700

    Update demo_view.py
    replaced code from bad merge

--- boots/views/demo_view.py
+++ boots/views/demo_view.py
@@ -4,10 +4,36 @@ This is really a throw away class We just need to view
 what we are working on
 """
 
-from django.shortcuts import render_to_response
+
 from boots.views.base import BootsFixedContainerView
-from boots.forms import DemoForm
 from boots.elements import display_elements
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic import View
+from boots.forms import forms as boots_forms
+
+
+class ErrorDemo(View):
+
+    def get(self, request):
+
+        form = boots_forms.ErrorExample()
+        context = RequestContext(request)
+        return render_to_response('boots/error.html',
+                                  {'form': form},
+                                  context)
+
+
+    def post(self, request):
+        form = boots_forms.ErrorExample(request.POST)
+        print request.POST
+        if not form.is_valid():
+            context = RequestContext(request)
+            return render_to_response('boots/error.html',
+                                      {'form': form},
+                                      context)
+
+
 
 
 # noinspection PyUnusedLocal,PyUnresolvedReferences
@@ -21,7 +47,7 @@ class DemoView(BootsFixedContainerView):
         Just a simple demo view for the widgets
         :param request:
         """
-        form = DemoForm()
+        form = boots_form.DemoForm()
         title = 'DjangoBoots Demo Page!'
         sub = 'Its a work in progress'
         page_header = display_elements.PageHeader(title, sub=sub)
