08ee72ebeb6fa1486b8b112b296ed3ef08d44c9a
root <root@mail.thenutbluff.com>
2015-04-26 15:10:28 -0700

    fixed wsgi to load correct settings

--- boots/wsgi.py
+++ boots/wsgi.py
@@ -7,9 +7,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
+import sys
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boots.settings")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "boots.settings.dev"
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
